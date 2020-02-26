a90e13746eca06d105dc4b851e728558bfd77f3a
agusnurwanto <agusnurwantomuslim@gmail.com>
2020-02-27 06:37:16 +0700

    tambahan info

---
+++ admin/partials/tentang-ppdb.php
@@ -0,0 +1,27 @@
+<h3>Membuat Tahun Ajaran Baru</h3>
+<ol>
+	<li>
+		Tambah tahun ajaran baru
+		<ul>
+			<li>PPDB Online > Pilih Tahun Ajaran (belum)</li>
+		</ul>
+	</li>
+	<li>
+		Membuat User Role baru misal "Calon Siswa Baru [tahun ajaran]". (Ganti tahun ajaran dengan tahun ajaran yang berlaku)
+		<ul>
+			<li>Ultimate Member > User Roles > Add New</li>
+		</ul>
+	</li>
+	<li>
+		Setting default user registration
+		<ul>
+			<li>Rubah settingan global wordpress di: Settings > General > New User Default Role</li>
+			<li>Pastikan settingan user registration di plugin Ultimate Member juga sudah sesuai: Ultimate Member > Settings > Apperance > Registration Form > Registration Default Role</li>
+		</ul>
+	</li>
+</ol>
+<h3>Catatan</h3>
+<ul>
+	<li>Plugin ini masih dalam pengembangan</li>
+	<li>Source code berasal dari repo <a href="https://github.com/agusnurwanto/ppdb-online" target="_blank">https://github.com/agusnurwanto/ppdb-online</a> atau bisa hubungi <a href="https://api.whatsapp.com/send?phone=6289629796473&text=Aplikasi%20PPDB-Online">Agus Nurwanto (0896-2979-6473)</a></li>
+</ul>
