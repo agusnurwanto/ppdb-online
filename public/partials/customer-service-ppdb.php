02f1ab109eb3fa975c24641bd7879b5f02928903
Agus Nurwanto <agusnurwantomuslim@gmail.com>
2024-12-08 22:02:20 +0700

    perbaikan col-6

--- public/partials/customer-service-ppdb.php
+++ public/partials/customer-service-ppdb.php
@@ -20,10 +20,10 @@ if (!defined('WPINC')) {
         </div>
     </div>
     <div class="row text-center" style="margin-bottom: 20px;">
-        <div class="col-md-6">
+        <div class="col-6">
             <a target="_blank" href="<?php echo $this->functions->generateWALink(get_option('_crb_ppdb_no_wa_1'), get_option('_crb_ppdb_pesan_wa_1')); ?>" class="btn btn-success btn-custom"><span class="dashicons dashicons-whatsapp"></span><br><?php echo get_option('_crb_ppdb_nama_wa_1'); ?></a>
         </div>
-        <div class="col-md-6">
+        <div class="col-6">
             <a target="_blank" href="<?php echo $this->functions->generateWALink(get_option('_crb_ppdb_no_wa_2'), get_option('_crb_ppdb_pesan_wa_2')); ?>" class="btn btn-success btn-custom"><span class="dashicons dashicons-whatsapp"></span><br><?php echo get_option('_crb_ppdb_nama_wa_2'); ?></a>
         </div>
     </div>
