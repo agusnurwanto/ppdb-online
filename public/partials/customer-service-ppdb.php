f9a02c5c522b059aadc4acf0fe997e8383aab96e
Agus Nurwanto <agusnurwantomuslim@gmail.com>
2024-12-08 19:02:42 +0700

    perbaikan hubungi admin

--- public/partials/customer-service-ppdb.php
+++ public/partials/customer-service-ppdb.php
@@ -14,10 +14,10 @@ if (!defined('WPINC')) {
     </div>
     <div class="row text-center" style="margin-bottom: 20px;">
         <div class="col-md-6">
-            <a target="_blank"> href="<?php echo $this->functions->generateWALink(get_option('_crb_ppdb_no_wa_1'), get_option('_crb_ppdb_pesan_wa_1')); ?>" class="btn btn-success btn-custom"><?php echo get_option('_crb_ppdb_nama_wa_1'); ?></a>
+            <a target="_blank" href="<?php echo $this->functions->generateWALink(get_option('_crb_ppdb_no_wa_1'), get_option('_crb_ppdb_pesan_wa_1')); ?>" class="btn btn-success btn-custom"><?php echo get_option('_crb_ppdb_nama_wa_1'); ?></a>
         </div>
         <div class="col-md-6">
-            <a target="_blank"> href="<?php echo $this->functions->generateWALink(get_option('_crb_ppdb_no_wa_2'), get_option('_crb_ppdb_pesan_wa_2')); ?>" class="btn btn-success btn-custom"><?php echo get_option('_crb_ppdb_nama_wa_2'); ?></a>
+            <a target="_blank" href="<?php echo $this->functions->generateWALink(get_option('_crb_ppdb_no_wa_2'), get_option('_crb_ppdb_pesan_wa_2')); ?>" class="btn btn-success btn-custom"><?php echo get_option('_crb_ppdb_nama_wa_2'); ?></a>
         </div>
     </div>
 </div>
